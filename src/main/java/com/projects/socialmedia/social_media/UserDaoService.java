e0e15f6ab35d3534ca6e31f092f42c246f71f282
mohammed_git <mohammedafridi62@gmail.com>
2026-02-09 01:34:26 +0530

    getalluser, getoneuser, createuser

---
+++ src/main/java/com/projects/socialmedia/social_media/UserDaoService.java
@@ -0,0 +1,37 @@
+package com.projects.socialmedia.social_media;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+@Component
+public class UserDaoService {
+    private static List<User> users = new ArrayList<>();
+
+    private static int userscount = 3;
+
+    static{
+        users.add(new User(++userscount,"Adam", LocalDate.now().minusYears(30)));
+        users.add(new User(userscount,"Goli", LocalDate.now().minusYears(23)));
+        users.add(new User(userscount,"Baje", LocalDate.now().minusYears(25)));
+    }
+
+    public List<User> findAll(){
+        return users;
+    }
+
+    public User findOne(int id){
+        // here first thought which comes is how to do , Normal programmer will use
+        // for loop on user and get the right user
+        Predicate<? super User> predicate = user -> user.getId().equals(id);
+         return users.stream().filter(predicate).findFirst().get();
+    }
+
+    public User save(User user){
+        users.add(user);
+        return user;
+    }
+}
---
+++ src/main/java/com/projects/socialmedia/social_media/UserResource.java
@@ -0,0 +1,35 @@
+package com.projects.socialmedia.social_media;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class UserResource {
+
+    // Because UserDaoService is a component managed by spring autowire it
+
+    private UserDaoService userDaoService;
+
+    public UserResource(UserDaoService userDaoService) {
+        this.userDaoService = userDaoService;
+    }
+
+    // GET Users
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers(){
+        return  userDaoService.findAll();
+    }
+
+    // GET Speicific user
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id){
+        return  userDaoService.findOne(id);
+    }
+
+    //POST User
+    @PostMapping("/users")
+    public void CreateUser(@RequestBody User user){
+
+    }
+}
